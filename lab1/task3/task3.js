9eda8f9cf911bc73e998f366068a6498173b9263
DzividzinskaM <marianadzividzinska04@gmail.com>
2021-12-11 15:24:23 +0200

    lab1: added task4

--- lab1/task3/task3.js
+++ lab1/task3/task3.js
@@ -165,4 +165,164 @@ const task3 = () => {
   bests.forEach((e) => console.log(e, decode(CIPHER, e.chromosome)));
 };
 
-task3();
+// task3();
+
+
+const CIPHER_MULTI =
+  'UMUPLYRXOYRCKTYYPDYZTOUYDZHYJYUNTOMYTOLTKAOHOKZCMKAVZDYBRORPTHQLSERUOERMKZGQJOIDJUDNDZATUVOTTLMQBOWNMERQTDTUFKZCMTAZMEOJJJOXMERKJHACMTAZATIZOEPPJKIJJNOCFEPLFBUNQHHPPKYYKQAZKTOTIKZNXPGQZQAZKTOTIZYNIUISZIAELMKSJOYUYYTHNEIEOESULOXLUEYGBEUGJLHAJTGGOEOSMJHNFJALFBOHOKAGPTIHKNMKTOUUUMUQUDATUEIRBKYUQTWKJKZNLDRZBLTJJJIDJYSULJARKHKUKBISBLTOJRATIOITHYULFBITOVHRZIAXFDRNIORLZEYUUJGEBEYLNMYCZDITKUXSJEJCFEUGJJOTQEZNORPNUDPNQIAYPEDYPDYTJAIGJYUZBLTJJYYNTMSEJYFNKHOTJARNLHHRXDUPZIALZEDUYAOSBBITKKYLXKZNQEYKKZTOKHWCOLKURTXSKKAGZEPLSYHTMKRKJIIQZDTNHDYXMEIRMROGJYUMHMDNZIOTQEKURTXSKKAGZEPLSYHTMKRKJIIQZDTNROAUYLOTIMDQJYQXZDPUMYMYPYRQNYFNUYUJJEBEOMDNIYUOHYYYJHAOQDRKKZRRJEPCFNRKJUHSJOIRQYDZBKZURKDNNEOYBTKYPEJCMKOAJORKTKJLFIOQHYPNBTAVZEUOBTKKBOWSBKOSKZUOZIHQSLIJJMSURHYZJJZUKOAYKNIYKKZNHMITBTRKBOPNUYPNTTPOKKZNKKZNLKZCFNYTKKQNUYGQJKZNXYDNJYYMEZRJJJOXMERKJVOSJIOSIQAGTZYNZIOYSMOHQDTHMEDWJKIULNOTBCALFBJNTOGSJKZNEEYYKUIXLEUNLNHNMYUOMWHHOOQNUYGQJKZLZJZLOLATSEHQKTAYPYRZJYDNQDTHBTKYKYFGJRRUFEWNTHAXFAHHODUPZMXUMKXUFEOTIMUNQIHGPAACFKATIKIZBTOTIKZNKKZNLORUKMLLFBUUQKZNLEOHIEOHEDRHXOTLMIRKLEAHUYXCZYTGUYXCZYTIUYXCZYTCVJOEBKOHE';
+
+const getNth = (str, k, b) =>
+  [...str].filter((_, i) => (i - b) % k === 0).join('');
+
+const concat = (strs) =>
+  [...strs[0]].map((_, i) => strs.map((str) => str[i]).join('')).join('');
+
+
+const getKeyHole = (str) => {
+  const counts = getLetterCounts(str);
+
+  return counts
+    .sort((a, b) => b.count - a.count)
+    .map((e) => e.letter)
+    .join('');
+};
+
+
+const decodeMulti = (str, keys) => {
+  return concat(
+    keys.map((key, i) => {
+      const strPart = getNth(str, keys.length, i);
+
+      const keyHole = getKeyHole(strPart);
+
+      const dict = [...keyHole].reduce(
+        (acc, hole, i) => ((acc[hole] = key[i]), acc),
+        {}
+      );
+
+      return transform(strPart, dict);
+    })
+  );
+};
+
+const algos = [];
+const best = [];
+const fitnessMulti = (i) => (phenotype) => {
+  const keys = best.map((b, j) => (i === j ? phenotype : b));
+
+  return (
+    0.3 *
+      nGram(1)(decodeMulti(CIPHER_MULTI, keys))
+        .map((gram) => SINGLE[gram])
+        .reduce((a, v) => a + (Math.log(v) || 0), 0) +
+    0.1 *
+      nGram(2)(decodeMulti(CIPHER_MULTI, keys))
+        .map((gram) => BIGRAMS[gram])
+        .reduce((a, v) => a + (Math.log(v) || 0), 0) +
+    0.3 *
+      nGram(3)(decodeMulti(CIPHER_MULTI, keys))
+        .map((gram) => TRIGRAMS[gram])
+        .reduce((a, v) => a + (Math.log(v) || 0), 0)
+  );
+};
+
+const configMulti = (i) => ({
+  mutationFn: mutate,
+  crossoverFn: crossover,
+  fitnessFn: fitnessMulti(i),
+  populationArr: [start, reverse],
+  populationSizeNum: 30, // defaults to 100
+});
+
+const create = (i) => {
+  const geneticalgorithm = require('../task3/GA')(configMulti(i));
+  algos[i] = geneticalgorithm;
+  best[i] = start;
+};
+
+// const run = async (i) => {
+//   // for (let j = 0; j < 1000; j++) {
+//   algos[i] = algos[i].evolve();
+//   // }
+//   // console.log(Array(10000).fill(null).map((e, i) => i).reduce((a, v) => (console.log(v), a.evolve()), geneticalgorithm).scoredPopulation())
+// };
+
+const task4 = (k) => {
+  const getLetterCounts = (str) => {
+    const plaintext = str.toLowerCase().replace(/[^a-z]/g, '');
+    const counts = new Array(26).fill(0);
+    let total = 0;
+
+    for (let i = 0; i < plaintext.length; i++) {
+      counts[plaintext.charCodeAt(i) - 97]++;
+      total++;
+    }
+
+    return { counts, total };
+  };
+
+  const getIC = (str) => {
+    const { counts, total } = getLetterCounts(str);
+
+    const sum = counts.reduce((a, v) => a + v * (v - 1), 0);
+    const ic = sum / (total * (total - 1));
+
+    return ic;
+  };
+
+
+  let i = 2;
+  const lengths = [];
+
+  for (i = 2; i < 20; i++) {
+    const ics = Array(i)
+      .fill(null)
+      .map((e, j) => getIC(getNth(CIPHER_MULTI, i, j)));
+    const ic = ics.reduce((a, v) => a + v) / ics.length;
+
+    lengths.push({ length: i, ic });
+  }
+
+  const maxIC = Math.max(...lengths.map((e) => e.ic));
+  const bestLength = lengths
+    .sort((a, b) => b.ic - a.ic)
+    .filter((e) => e.ic > maxIC * 0.9)
+    .filter(
+      (e, _, arr) =>
+        !arr.some((s) => e.length !== s.length && e.length % s.length === 0)
+    );
+
+  const keylen = bestLength[0].length;
+  console.log('key length', keylen);
+
+  for (let i = 0; i < keylen; i++) {
+    console.log('create', i);
+    create(i);
+  }
+
+  let j = 1;
+  const max = { f: algos[0].maxFitness, j: 1 };
+
+  while (max.j + 100 > j && j < 10000) {
+    for (let i = 0; i < keylen; i++) {
+      algos[i].iterate();
+      if (algos[i].maxFitness > max.f) {
+        max.f = algos[i].maxFitness;
+        max.j = j;
+      }
+    }
+    if (j % k === 0) {
+      algos.forEach((algo, i) => (best[i] = algo.getBest()[0].chromosome));
+    }
+    j++;
+  }
+
+  for (let i = 0; i < keylen; i++) {
+    const bestie = algos[i].getBest()[0].chromosome;
+    best[i] = bestie;
+    console.log({ i, code: bestie, score: fitnessMulti(i)(bestie) });
+  }
+  console.log(decodeMulti(CIPHER_MULTI, best));
+};
+
+task4(50);
